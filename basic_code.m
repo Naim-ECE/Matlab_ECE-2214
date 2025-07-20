01d44de2eec06bda467b6f6440f2c618574e058a
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-07-20 21:22:46 +0600

    basic matlab code

---
+++ basic_code.m
@@ -0,0 +1,12 @@
+% Basic arithmetic
+a = 5;
+b = 3;
+sum = a + b;
+difference = a - b;
+product = a * b;
+quotient = a / b;
+
+disp(['Sum: ', num2str(sum)]);
+disp(['Difference: ', num2str(difference)]);
+disp(['Product: ', num2str(product)]);
+disp(['Quotient: ', num2str(quotient)]);
